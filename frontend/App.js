686fc08b0a6488dc35354a1b09c8599bf1c45e15
Sherryll Elliott <sherryllelliott@gmail.com>
2021-01-27 19:35:15 +0000

    components imported

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Switch, Link, Route } from 'react-router-dom'
 import './styles/style.scss'
+import Arrears from './components/Arrears'
+import Advice from './components/Advice'
+import Becomelandlord from './components/Becomelandlord'
+import Deposits from './components/Deposits'
+import Discrimination from './components/Discrimination'
+import Maintenance from './components/Maintenance'
+import Signinglease from './components/Signinglease'
+import Vetting from './components/Vetting'
+
+
 
 // ! Some starter code for your frontends, change this
 // ! however you like.
@@ -9,6 +19,14 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/hello/world" component={MyPage} />
+      <Route exact path="" component={} />
+      <Route exact path="" component={} />
+      <Route exact path="" component={} />
+      <Route exact path="" component={} />
+      <Route exact path="" component={} />
+      <Route exact path="" component={} />
+      <Route exact path="" component={} />
+      <Route exact path="" component={} />
     </Switch>
   </BrowserRouter>
 )
